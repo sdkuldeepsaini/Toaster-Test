50fe74ff751a4b2d02a77a96f1351fd73d243052
kuldeeplogiq <sdkuldeepsaini@gmail.com>
2018-12-04 12:10:23 +0530

    docs

--- toastlibrary/src/main/java/com/kuldeepr/user/toastlibrary/ToasterMessage.kt
+++ toastlibrary/src/main/java/com/kuldeepr/user/toastlibrary/ToasterMessage.kt
@@ -3,17 +3,18 @@ package com.kuldeepr.user.toastlibrary
 import android.content.Context
 import android.widget.Toast
 
-/**
- * @author Kuldeep
- * @param c
- * @param message
- * Show the toast message
- */
 
 
 class ToasterMessage {
 
+
     companion object {
+        /**
+         * @author Kuldeep
+         * @param c
+         * @param message
+         * Show the toast message
+         */
 
         public fun S(c: Context, message:String){
             Toast.makeText(c,message,Toast.LENGTH_LONG).show()
